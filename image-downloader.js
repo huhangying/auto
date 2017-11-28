4aaaff9d8b060263d0a026f84663f0332877c83c
Harry Hu <hangying.hu@ericsson.com>
2017-11-28 14:56:32 -0500

    enhanced getting the list.

---
+++ image-downloader.js
@@ -0,0 +1,25 @@
+const download = require('image-downloader')
+
+
+async function downloadIMG(url) {
+    try {
+        const { filename, image } = await download.image({url: url, dest: '.'})
+        console.log('File saved to', filename);
+    } catch (e) {
+        throw e
+    }
+}
+
+let list = [
+    'http://www.moremorewin.net/imgserver/imgs/2017/11/21/35f36d3c90228a27036fcf64ef9faf1c.jpg',
+    'http://www.moremorewin.net/imgserver/imgs/2017/11/21/bb5c57d86ff9180f3072fb594ae91981.jpg'
+];
+
+console.log(new Date())
+list.map(function(_url) {
+    downloadIMG(_url).then(function() {
+        console.log(new Date())
+    })
+})
+
+
--- util/util.js
+++ util/util.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var rp = require('request-promise');
 
 var baseUrl = 'http://www.popyard.com/cgi-mod';
 
@@ -16,32 +16,12 @@ var fetch = function (url) {
     url = getFullUrl(url);
     console.log('Processing', url);
 
-    return new global.Promise(function (resolve, reject) {
-        request(
-            {
-                headers: {
-                    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                    //'accept-encoding': 'gzip, deflate',
-                    'accept-language': 'en-US,en;q=0.8',
-                    //'cache-control':'max-age=0',
-                    //'connection':'keep-alive',
-                    //'cookie':'ushudie=CA; __gads=ID=4b545c990bd67bf3:T=1468951586:S=ALNI_MaJu88dfKO5daAOAtdleI-vvpj9ig; __qca=P0-2044153097-1502891285865; banner_728_90_hmh_9_5_17_silencer_728x90_banner_unique_user_tracking=1; blogads_book_excerpt_client_side=100473135%7C1; banner_160_600_scholastic_10_1_17_about_mia_160x600_banner_unique_user_tracking=1; _ga=GA1.2.871092476.1468951542; __sonar=6456986047480514964; _bs5a46dcd961caa48998d460c1d64df5c0=1; __utmt=1; __utma=30831166.871092476.1468951542.1510762734.1510770524.351; __utmb=30831166.3.10.1510770524; __utmc=30831166; __utmz=30831166.1510720697.349.343.utmcsr=popyard.org|utmccn=(referral)|utmcmd=referral|utmcct=/',
-                    //'host':'www.popyard.com',
-                    //'upgrade-insecure-requests':'1',
-                    'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-                },
-                uri: url,
-                method: 'GET'
-            },
-            function (error, response, html) {
-                if(!error) {
-                    //var $ = cheerio.load(html);
-                    resolve(html);
-                }
-                else {
-                    reject(error);
-                }
-            });
+    return rp({
+        uri: url,
+        headers: {
+            'accept-language': 'en-US,en;q=0.8',
+            'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+        }
     });
 };
 
