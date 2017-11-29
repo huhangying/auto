6ec952167f0daa2a7b669da22680ab91d77ec18c
Harry Hu <hangying.hu@ericsson.com>
2017-11-29 16:19:07 -0500

    commit

--- image-downloader.js
+++ image-downloader.js
@@ -1,27 +1,28 @@
 const download = require('image-downloader')
 
 
-async function downloadIMG(url) {
-    try {
-        const { filename, image } = await download.image({url: url, dest: '.'})
-        console.log('File saved to', filename);
-    } catch (e) {
-        throw e
-    }
-}
 
 let list = [
     'http://www.moremorewin.net/imgserver/imgs/2017/11/21/35f36d3c90228a27036fcf64ef9faf1c.jpg',
     'http://www.moremorewin.net/imgserver/imgs/2017/11/21/bb5c57d86ff9180f3072fb594ae91981.jpg'
 ];
 
-console.log(new Date())
-list.map(function(_url) {
-    downloadIMG(_url)
-        .then(() => {
-            console.log(new Date())
+console.log(new Date());
+
+list.map(
+    async (url) => {
+        await download.image({
+            headers: {
+                'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+            },
+            url: url,
+            dest: '.'// image保存的目录
         })
-        .catch(err => console.log(err.stack));
-})
+            .then(({filename}) => {
+                console.log(`File saved to ${filename} @ ${(new Date()).toISOString()}`);
+            })
+            .catch(err => console.log(err.stack));
+    }
+);
 
 
--- util/util.js
+++ util/util.js
@@ -19,7 +19,7 @@ var fetch = async function (url) {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             //'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'en-US,en;q=0.9',
-            'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
         }
     };
 
