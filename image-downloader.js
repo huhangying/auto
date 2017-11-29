cd1aab73a784197d32f7b84030cc0a996242b281
Harry Hu <hangying.hu@ericsson.com>
2017-11-29 17:05:20 -0500

    commit

--- image-downloader.js
+++ image-downloader.js
@@ -13,6 +13,7 @@ list.map(
     async (url) => {
         await download.image({
             headers: {
+                'Accept': 'image/webp,image/apng,image/jpeg,image/*,*/*;q=0.8',
                 //'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
                 'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24'
             },
