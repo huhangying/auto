6349b9bd5a5c363b143c6a50eff3673627c9a8cb
Harry Hu <hangying.hu@ericsson.com>
2017-12-15 14:06:28 -0500

    test

--- image-downloader.js
+++ image-downloader.js
@@ -11,7 +11,7 @@ console.log(new Date());
 
 list.map(
     async (url) => {
-        await download.image({
+        return await download.image({
             headers: {
                 'Accept': 'image/webp,image/apng,image/jpeg,image/*,*/*;q=0.8',
                 //'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
