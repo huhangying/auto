06fd29b61ce52f57c109b4a484d69d0004ab5b6b
Harry Hu <hangying.hu@ericsson.com>
2017-12-15 12:02:00 -0500

    commit

--- util/util.js
+++ util/util.js
@@ -1,7 +1,8 @@
 //var rp = require('request-promise').defaults({ simple: false });
 let axios = require('axios');
+let http = require('http');
 
-const baseUrl = 'http://www.popyard.com/cgi-mod';
+const baseUrl = 'http://www.popyard.com/cgi-mod/';
 
 var getFullUrl = function(str) {
     if (str.indexOf(baseUrl) < 0) {
@@ -10,32 +11,51 @@ var getFullUrl = function(str) {
     return str;
 };
 
+var getUri = function(str) {
+    if (str.indexOf(baseUrl) >= 0) {
+        return str.substring(baseUrl.length-1) + '&v=0&k=0';
+    }
+    return str;
+};
+
 var fetch = async function (url) {
-    url = getFullUrl(url);
+    url = getUri(url);
+    //url = getFullUrl(url);
     console.log('Processing', url);
 
     let options = {
-        uri: url,
+        baseURL: baseUrl,
+        url: url,
         headers: {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             //'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'en-US,en;q=0.9',
-            //'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-            'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24'
+            //'Accept-Language': 'en-US,en;q=0.9',
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+            //'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24'
         },
-        maxAttempts : 15,
-        retryDelay : 2000,
-        followRedirect : false,
-        followAllRedirects : false
+        httpAgent: new http.Agent({ keepAlive: true })
+        // httpsAgent: new https.Agent({ keepAlive: true }),
+        // maxAttempts : 15,
+        // retryDelay : 2000,
+        // followRedirect : false,
+        // followAllRedirects : false
     };
 
-    return await axios.get(url)
+    return await axios(options)
     //return await rp(options)
-        .then(htmlString => {
+        .then(response => {
             //console.log(`-----> get page length: ${htmlString.data}`);
-            return htmlString.data;
+            if (response.status === 200) {
+                return response.data;
+            }
+            else {
+                return `<html></html>`;
+            }
+
         })
-        .catch(error => console.error(error.stack));
+        .catch(error => {
+            console.error(error.stack)
+        });
 };
 
 module.exports = {
