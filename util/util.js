67d9a149932906c2d7f693b666490d40f6cf3e84
Harry Hu <hangying.hu@ericsson.com>
2017-12-18 15:32:11 -0500

    test

--- util/util.js
+++ util/util.js
@@ -19,31 +19,30 @@ var getUri = function(str) {
 };
 
 var fetch = async function (url) {
-    url = getUri(url);
-    //url = getFullUrl(url);
+    //url = getUri(url);
+    url = getFullUrl(url);
     console.log('Processing', url);
 
     let options = {
-        baseURL: baseUrl,
-        url: url,
+        // baseURL: baseUrl,
+        // url: url,
         headers: {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             //'Accept-Encoding': 'gzip, deflate',
             //'Accept-Language': 'en-US,en;q=0.9',
-            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
-            //'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24'
+            //'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+            'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24'
         },
         // transformResponse: [function (data) {
         //     // Do whatever you want to transform the data
         //
         //     return data;
         // }],
-        // timeout: 1000,
+        timeout: 2000,
         validateStatus: function (status) {
             //return status >= 200 && status < 300; // default
             return true;
         },
-        responseType: 'text',
         httpAgent: new http.Agent({ keepAlive: true })
         // httpsAgent: new https.Agent({ keepAlive: true }),
         // maxAttempts : 15,
@@ -52,7 +51,7 @@ var fetch = async function (url) {
         // followAllRedirects : false
     };
 
-    return await axios(options)
+    return await axios.get(url, options)
     //return await rp(options)
         .then(response => {
             //console.log(`-----> get page length: ${htmlString.data}`);
