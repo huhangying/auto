5cec393b32db8de072f975de40cf135c55ebbf9d
Harry Hu <hangying.hu@ericsson.com>
2018-01-11 17:17:53 -0500

    use proxy

--- util/util.js
+++ util/util.js
@@ -42,10 +42,10 @@ var fetch = async function (url) {
             //'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
             'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24'
         },
-        // proxy: {
-        //     host: '54.197.236.138',
-        //     port: 8080
-        // },
+        proxy: {
+            host: '54.197.236.138',
+            port: 8080
+        },
         // transformResponse: [function (data) {
         //     // Do whatever you want to transform the data
         //
