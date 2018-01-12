9861a7be06537551ac49fbb39c2c98155ad88a59
Harry <hhu@live.ca>
2018-01-11 22:15:30 -0500

    no proxy

--- util/util.js
+++ util/util.js
@@ -34,7 +34,7 @@ var fetch = async function (url) {
         // baseURL: baseUrl,
         // url: url,
         headers: {
-            'Host': 'www.popyard.com',
+            //'Host': 'www.popyard.com',
             //'Accept': '*/*',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             //'Accept-Encoding': 'gzip, deflate',
@@ -42,16 +42,16 @@ var fetch = async function (url) {
             //'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
             'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24'
         },
-        proxy: {
-            host: '178.22.148.122',
-            port: 3129
-        },
+        // proxy: {
+        //     host: '178.22.148.122',
+        //     port: 3129
+        // },
         // transformResponse: [function (data) {
         //     // Do whatever you want to transform the data
         //
         //     return data;
         // }],
-        timeout: 60000,
+        timeout: 40000,
         // validateStatus: function (status) {
         //     //return status >= 200 && status < 300; // default
         //     return true;
