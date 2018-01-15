24d579e0691351d180263a68bddfae9d3ebc490c
Harry Hu <hangying.hu@ericsson.com>
2018-01-15 15:29:41 -0500

    supported to get another proxy after failed.

--- util/util.js
+++ util/util.js
@@ -3,8 +3,12 @@ let http = require('http');
 require('axios-debug-log');
 
 const baseUrl = 'http://www.popyard.com/cgi-mod/';
-const maxTry = 3;
+const maxTry = 5;
 let currentTry = 0;
+let proxy = {
+    host: '66.82.144.29',
+    port: 8080
+};
 
 var getFullUrl = function(str) {
     if (str.indexOf(baseUrl) < 0) {
@@ -26,6 +30,15 @@ var getUri = function(str) {
     return str;
 };
 
+const getAProxy = async (url) => {
+    url = 'https://gimmeproxy.com/api/getProxy';
+    let proxy = await axios.get(url);
+    return {
+        host: proxy.data.ip,
+        port: proxy.data.port
+    }
+}
+
 //todo: support retry with different proxy when failed
 var fetch = async function (url) {
     //url = getUri(url);
@@ -38,12 +51,9 @@ var fetch = async function (url) {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'User-Agent':'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.2.24) Gecko/20111109 CentOS/3.6.24-3.el6.centos Firefox/3.6.24'
         },
-        proxy: {
-            host: '66.82.144.29',
-            port: 8080
-        },
+        proxy: proxy,
         timeout: 3000,
-        maxRedirects: 5, // default
+        maxRedirects: 0, // 5 by default
         responseType: 'text',
         httpAgent: new http.Agent({ keepAlive: true })
     };
@@ -57,14 +67,15 @@ var fetch = async function (url) {
                 return `<html></html>`;
             }
         })
-        .catch(error => {
+        .catch(async error => {
             if (currentTry < maxTry) {
+                console.error(`${url} tried ${currentTry} time. -- proxy: ${proxy.host}:${proxy.port}`);
                 currentTry++;
-                console.error(`${url} tried ${currentTry} time.`);
+                proxy = (await getAProxy()).data; // change to another proxy
                 return fetch(url);
             }
             else {
-                console.error(`${url} tried ${currentTry} time. -- failed.`);
+                console.error(`${url} tried ${currentTry} time. -- proxy: ${proxy.host}:${proxy.port} failed.`);
                 console.error(error.stack + '|' + error );
                 currentTry = 0;
                 return `<html>${error.stack}</html>`;
